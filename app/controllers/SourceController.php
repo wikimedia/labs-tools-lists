f29c1c9c496953a01fa4b3fc4edb6e4154813b75
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-02 23:10:50 +0200

    Move folderview code in view from the controller

--- app/controllers/SourceController.php
+++ app/controllers/SourceController.php
@@ -2,42 +2,13 @@
 
 class SourceController extends BaseController {
 
-    public static function listFolder($path)
-    {
-        $dir = "../query/" . $path;
-        $files = scandir($dir);
-        $out = "";
-        foreach ($files as $file)
-        {
-            switch ($file) {
-                case '.':
-                    break;
-                case '..':
-                    if ($path != "")
-                        if(strpos($path, "/"))
-                            $out .= "<a href=\"" . substr($path, 0, strrpos($path, "/")) . "\">" . $file . "</a><br />";
-                        else
-                            $out .= "<a href=\"/lists/src\">" . $file . "</a><br />";
-                    break;
-                default:
-                    if (strpos($file, "."))
-                        $out .= "<a href=\"" . $path . "/" . substr($file, 0, strpos($file, ".")) . "\">" . $file . "</a><br />";
-                    else
-                        $out .= "<a href=\"/lists/src/" . $path . "/" . $file . "\">" . $file . "</a><br />";
-                    break;
-            }
-        }
-        return $out;
-    }
-
     public function show($path = null)
     {
         if(is_dir("../query/" . $path)) // Check if it's a folder
-            echo SourceController::listFolder($path);
+            return View::make('folder')->with('path', $path);
         else
         {
-            return View::make('file')
-                ->with('file', $path);
+            return View::make('file')->with('file', $path);
         }
     }
 
---
+++ app/views/folder.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('title')File - {{$path}}@stop
+
+@section('content')
+    <h1>{{$path}}</h1>
+    <?php
+	    $dir = "../query/" . $path;
+        $files = scandir($dir);
+        $out = "";
+        if ($path != "")
+            if(strpos($path, "/"))
+                $out .= "<a href=\"" . substr($path, 0, strrpos($path, "/")) . "\">..</a><br />";
+            else
+                $out .= "<a href=\"/lists/src\">..</a><br />";
+        foreach ($files as $file)
+        {
+            if ($file != "." AND $file != "..") {
+                if (strpos($file, "."))
+                    $out .= "<a href=\"" . $path . "/" . substr($file, 0, strpos($file, ".")) . "\">" . $file . "</a><br />";
+                else
+                    $out .= "<a href=\"/lists/src/" . $path . "/" . $file . "\">" . $file . "</a><br />";
+                break;
+            }
+        }
+        echo $out;
+    ?>
+
+@stop
