ad6c070060f35ce34b15c7c542a037a7686c216c
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-08 23:05:12 +0200

    Add SourceController::linkedPath(PATH) function to create nicer looking (and more functional) paths

--- app/controllers/SourceController.php
+++ app/controllers/SourceController.php
@@ -8,7 +8,9 @@ class SourceController extends BaseController {
             return View::make('folder')->with('path', $path);
         else
         {
-            return View::make('file')->with('file', $path);
+            return View::make('file')
+            	->with('file', $path)
+            	->with('path', $path);
         }
     }
 
@@ -18,5 +20,25 @@ class SourceController extends BaseController {
   		unset($elements[0]);
   		unset($elements[1]);
   		return $elements;
+    }
+
+    public static function linkedPath($path)
+    {
+    	$array = explode("/", $path);
+    	$out = "";
+    	$last = "/lists";
+    	$lastElementKey = sizeof($array) - 1;
+
+    	foreach ($array as $k => $v) {
+    		if ($k < $lastElementKey) {
+    			$url = $last . "/" . $v;
+	    		$out .= "<a href=\"" . $url . "\">" . $v . "</a>/";
+    			$last = $url;
+    		}
+    	}
+
+    	$out .= $array[$lastElementKey];
+    	return $out;
+
     }
 }
