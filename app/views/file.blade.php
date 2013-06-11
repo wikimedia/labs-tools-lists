355df6e2da92c7475d1d975fc47528b79e2e7c57
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-11 18:34:38 +0200

    Add titles to file view

--- app/views/file.blade.php
+++ app/views/file.blade.php
@@ -4,10 +4,13 @@
 
 @section('content')
     <h1>{{SourceController::linkedPath($path)}}</h1>
+
+    <h2>Query</h2>
     <?php
 	    $source = file_get_contents(base_path() . "/query/" . $file . ".sql");
     	$geshi = new GeSHi($source, 'sql');
     	echo $geshi->parse_code();
     ?>
 
+    <h2>Output</h2>
 @stop
