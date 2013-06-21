34cc97f1f4ed171315b7d5449b1f535115777bdb
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-21 15:59:21 +0200

    Add queries table

---
+++ app/database/migrations/2013_06_21_221202_create_queries.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQueries extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('queries', function($table)
+		{
+    		$table->increments('id');
+    		$table->string('name');
+    		$table->timestamp('last_execution_at');
+    		$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('queries');
+	}
+
+}
