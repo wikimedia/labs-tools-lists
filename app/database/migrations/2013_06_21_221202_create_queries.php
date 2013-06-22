398fea8f9697c2112382c28054484580b93aa960
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-22 18:24:10 +0000

    Add times column

--- app/database/migrations/2013_06_21_221202_create_queries.php
+++ app/database/migrations/2013_06_21_221202_create_queries.php
@@ -16,6 +16,7 @@ class CreateQueries extends Migration {
     		$table->increments('id');
     		$table->string('name');
     		$table->timestamp('last_execution_at');
+    		$table->integer('times');
     		$table->timestamps();
 		});
 	}
