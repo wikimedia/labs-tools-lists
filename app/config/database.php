73ee5c53febe420cb4740166d99a7f05381ce382
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-22 19:16:54 +0000

    Clean from unused db and add itwiki db (more will come when needed)

--- app/config/database.php
+++ app/config/database.php
@@ -48,12 +48,6 @@ return array(
 
 	'connections' => array(
 
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
-			'prefix'   => '',
-		),
-
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'tools-db',
@@ -65,24 +59,15 @@ return array(
 			'prefix'    => '',
 		),
 
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
+		'itwiki' => array(
+			'driver'    => 'mysql',
+			'host'      => 'itwiki.labsdb',
+			'database'  => 'itwiki_p',
+			'username'  => $config['user'],
+			'password'  => $config['password'],
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
 		),
 
 	),
