c7ed895f2174f6637873b334c2a50f2c5a651d30
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-25 22:49:03 +0000

    Wrong filename

--- app/views/file.blade.php
+++ app/views/file.blade.php
@@ -17,8 +17,12 @@
 
     <h2>Output ({{$object->last_execution_results}})</h2>
     <?php
+    	$outputs = SourceController::getDir("output/" . $path);
     	$filename = Execution::getSafeDate($object->last_execution_at) . ".out";
+	    $source = file_get_contents(base_path() . "/output/" . $file . "/" . $filename);
     ?>
     <pre class="sql" style="font-family:monospace;">{{SourceController::cleanWikiCode(trim($source))}}</pre>
+
+    ?>
 
 @stop
