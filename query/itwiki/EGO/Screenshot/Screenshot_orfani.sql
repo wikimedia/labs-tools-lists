2dfd69dac6914965385544348800c9390c2e8691
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-06-29 10:16:28 +0000

    Now the query is ready for production

--- query/itwiki/EGO/Screenshot/Screenshot_orfani.sql
+++ query/itwiki/EGO/Screenshot/Screenshot_orfani.sql
@@ -1,8 +1,8 @@
+CONNECT itwiki_p itwiki.labsdb;
 SELECT concat("#[[:Immagine:", page.page_title, "]]")
   FROM page JOIN categorylinks ON page_id = cl_from
   WHERE cl_to LIKE 'Screenshot_copy%'
     AND page_namespace = 6
     AND page.page_title NOT IN (
       SELECT DISTINCT il_to FROM imagelinks
-    )
-  LIMIT 1;
+    );
