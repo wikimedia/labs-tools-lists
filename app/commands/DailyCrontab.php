524ec83323aca9f10f26438f5f439bd6a65e7328
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-07-13 22:34:29 +0000

    Fix license indication

--- app/commands/DailyCrontab.php
+++ app/commands/DailyCrontab.php
@@ -6,7 +6,8 @@ use Symfony\Component\Console\Input\InputArgument;
 
 /**
  * @author Fabio Alessandro Locati <fabiolocati@gmail.com>
- * @copyright AGPL-3.0
+ * @copyright Fabio Alessandro Locati 2013
+ * @license AGPL-3.0 http://www.gnu.org/licenses/agpl-3.0.html
  */
 class DailyCrontab extends Command {
 
