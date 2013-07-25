af4acacacd13e5057a34650a8382178bbfafea7f
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-07-25 11:26:41 +0000

    Add more doc

--- app/models/Execution.php
+++ app/models/Execution.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * @author Fabio Alessandro Locati <fabiolocati@gmail.com>
+ * @copyright Fabio Alessandro Locati 2013
+ * @license AGPL-3.0 http://www.gnu.org/licenses/agpl-3.0.html
+ */
 class Execution extends Eloquent{
 
+	/**
+	 * Fillable elements
+	 * @var array
+	 */
 	protected $fillable = array('query_id', 'time', 'duration', 'results');
+
+	/**
+	 * Timestamps on database
+	 * @var boolean
+	 */
 	public $timestamps = false;
 
+	/**
+	 * Transform standard date to FileSystem-safe date
+	 * @param  string $date The date to transform
+	 * @return string       Transformed date
+	 */
 	public static function getSafeDate($date)
 	{
 		return date('Ymd-His', strtotime($date));
