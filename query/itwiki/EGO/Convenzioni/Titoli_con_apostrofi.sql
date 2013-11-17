64d10246e8dc8c3430cf4db244463e1e994e1488
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-11-17 10:56:53 +0000

    Add itiki/EGO/Convenioni/Titoli_con_apostrofi

---
+++ query/itwiki/EGO/Convenzioni/Titoli_con_apostrofi.sql
@@ -0,0 +1,15 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT("# [[", page_title, "]]") FROM page
+  WHERE page_namespace = 0
+    AND page_is_redirect = 0
+    AND (page_title LIKE "%’%" 
+      OR page_title LIKE "%‘%"
+      OR page_title LIKE "%‚%"
+      OR page_title LIKE "%“%"
+      OR page_title LIKE "%”%"
+      OR page_title LIKE "%„%"
+      OR page_title LIKE "%–%"
+      OR page_title LIKE "%—%"
+      OR page_title LIKE "%…%"
+    )
+ORDER BY page_title;
