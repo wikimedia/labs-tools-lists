ed051fca61f8edf55296731238f7bf02ffd4e1fd
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-11-17 10:50:07 +0000

    Add itiki/EGO/Convenioni/O_nel_titolo/Voci query

---
+++ query/itwiki/EGO/Convenzioni/O_nel_titolo/Voci.sql
@@ -0,0 +1,10 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('*[[', page_title, ']]')
+  FROM page
+  WHERE page_namespace = 0 AND page_is_redirect = 0
+    AND page_title like '%\_o\_%'
+    AND page_title NOT IN (SELECT pl_title
+      FROM pagelinks JOIN page as p ON pl_from = p.page_id
+      WHERE p.page_namespace=4 
+        AND p.page_title = 'Elenchi_generati_offline/Voci_e_categorie_con_"o"_nel_titolo/Whitelist_voci')
+  ORDER BY page_title;
