0d2e142e1316ee6d86cd5c5bc52e737596dc9fa7
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-06 15:04:59 +0100

    Even more improvements for the layout file

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Fabio Alessandro Locati">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <style>
       body {
         padding-top: 50px;
@@ -27,34 +26,36 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="/lists">Lists</a>
-          <div class="collapse nav-collapse">
-            <p class="navbar-text pull-right">
-              <a href="/lists/docs" class="navbar-link">Docs</a>
-            </p>
-            <ul class="nav navbar-collapse">
-              @foreach (SourceController::getDir("query") as $file)
-                <li class="dropdown">
-                  <a href="/lists/{{$file}}">{{$file}}</a>
-                </li>
-              @endforeach
-            </ul>
-          </div>
+        </div>
+        <div class="collapse nav-collapse">
+          <p class="navbar-text pull-right">
+            <a href="/lists/docs" class="navbar-link">Docs</a>
+          </p>
+          <ul class="nav navbar-collapse">
+            @foreach (SourceController::getDir("query") as $file)
+              <li><a href="/lists/{{$file}}">{{$file}}</a></li>
+            @endforeach
+          </ul>
         </div>
       </div>
     </div>
+
     <div class="container">
       @yield('content')
     </div> <!-- /container -->
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-38831639-2']);
+        _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+
   </body>
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-  <script type="text/javascript">
-    var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-38831639-2']);
-      _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
 </html>
