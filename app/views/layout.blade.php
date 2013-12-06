65e0bd266c5a998f0afdef7c148c416bf07a9e41
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-06 14:54:23 +0100

    Fix layout file

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -9,25 +9,29 @@
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <style>
       body {
-        padding-top: 60px;
+        padding-top: 50px;
+      }
+      .starter-template {
+        padding: 40px 15px;
+        text-align: center;
       }
     </style>
   </head>
   <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="/lists">Lists</a>
-          <div class="nav-collapse collapse">
+          <a class="navbar-brand" href="/lists">Lists</a>
+          <div class="collapse nav-collapse">
             <p class="navbar-text pull-right">
               <a href="/lists/docs" class="navbar-link">Docs</a>
             </p>
-            <ul class="nav">
+            <ul class="nav navbar-collapse">
               @foreach (SourceController::getDir("query") as $file)
                 <li class="dropdown">
                   <a href="/lists/{{$file}}">{{$file}}</a>
