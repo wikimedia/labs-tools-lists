112981b3a4045e74b510103869d3a25f6f677901
alemela <alessiom92@gmail.com>
2014-02-23 00:36:14 +0100

    Add 2 lists in Voci

---
+++ query/itwiki/EGO/Voci/Voci_con_piu_redirect.sql
@@ -0,0 +1,8 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('* [[', rd_title, ']] - ', COUNT(*)) 
+ FROM redirect
+ JOIN page ON rd_from = page_id
+ WHERE page_namespace = 0
+ GROUP BY rd_title
+ ORDER BY COUNT(*) DESC
+ LIMIT 200;
