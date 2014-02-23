941657233b3db60ca705cc8042dfc8b891e7b728
alemela <alessiom92@gmail.com>
2014-02-23 18:01:59 +0100

    Fix & Add itwiki/EGO/Template/Template che puntano redirect

--- query/itwiki/EGO/Template/Template_che_puntano_redirect.sql
+++ query/itwiki/EGO/Template/Template_che_puntano_redirect.sql
@@ -1,8 +1,8 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT("# [[Template:", p1.page_title, "]] punta al redirect [[", p2.page_title, "]]") 
+SELECT CONCAT('# [[Template:', p1.page_title, ']] punta al redirect [[', p2.page_title, ']]') 
   FROM page AS p1 
     JOIN pagelinks ON p1.page_id = pl_from
-    JOIN page as p2 ON pl_title = p2.page_title 
+    JOIN page AS p2 ON pl_title = p2.page_title 
       AND pl_namespace = p2.page_namespace
   WHERE p1.page_namespace = 10 
     AND p2.page_namespace = 0 
