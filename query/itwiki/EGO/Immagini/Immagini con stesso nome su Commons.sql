990292cb0f2c500ab97ffd214f4150979a361513
bittachi <mesand16@gmail.com>
2014-03-02 16:11:14 +0100

    Update Immagini con stesso nome su Commons.sql

--- query/itwiki/EGO/Immagini/Immagini con stesso nome su Commons.sql
+++ query/itwiki/EGO/Immagini/Immagini con stesso nome su Commons.sql
@@ -1,7 +1,8 @@
 CONNECT itwiki_p itwiki.labsdb commonswiki_p commonswiki.labsdb;
-SELECT "# [[:File:", commonswiki.image.img_name, "|]] (su [[:commons:Image:",
-       commonswiki.image.img_name, "|commons]]): ", commonswiki.image.img_size
+SELECT "* [[:File:", commonswiki.image.img_name, "|]] (", itwiki.image.img_size,
+       "; su [[:commons:File:", commonswiki.image.img_name, "|commons]]: ", 
+       commonswiki.image.img_size, ")"       
 FROM commonswiki.image
 JOIN itwiki.image
 WHERE commonswiki.image.img_name = itwiki.image.img_name
-ORDER BY commonswiki.image.img_size DESC ;
+ORDER BY commonswiki.image.img_name ;
