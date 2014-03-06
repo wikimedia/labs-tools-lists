e56f21dbb7b3d662ee9450ba1112c3ac00eb42d2
incola <diegmonti@gmail.com>
2014-03-06 20:44:04 +0100

    Modify query/itwiki/EGO/Varie/Pagine_corte.sql

--- query/itwiki/EGO/Varie/Pagine_corte.sql
+++ query/itwiki/EGO/Varie/Pagine_corte.sql
@@ -3,5 +3,7 @@ SELECT CONCAT("# [[", ns_name, ":", page_title, "]] (", page_len, ")")
 FROM page
 JOIN p50380g50614__lists.ns_it ON page_namespace = ns_id
 WHERE page_namespace <> '0'
+AND page_namespace <> '2'
+AND page_namespace <> '3'
 AND page_len < '5'
 ORDER BY page_namespace, page_title;
