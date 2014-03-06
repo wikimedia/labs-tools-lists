adbf0053979154e96f73181763dbd4ccfed6f123
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-03-06 19:20:00 +0100

    Fix query + disable auto-running since it's too slow

--- query/itwiki/EGO/Immagini/Immagini duplicate su Commons.sql
+++ query/itwiki/EGO/Immagini/Immagini duplicate su Commons.sql
@@ -1,7 +1,7 @@
 CONNECT itwiki_p itwiki.labsdb;
 SELECT CONCAT( "#[[:File:", img1.img_name, "|]] identica a [[:commons:File:", img2.img_name, "|]]" )
 FROM image AS img1
-JOIN commonswiki_p.image AS img2 ON img1.img_sha1 = img2.img_sha1
+JOIN commonswiki_f_p.image AS img2 ON img1.img_sha1 = img2.img_sha1
 WHERE img1.img_sha1 <> ''
 AND img1.img_name NOT REGEXP 'Stub_.*\.png'
 AND img1.img_name NOT REGEXP '.*-Stemma\.png'
