621686849f13af5478ff1dcd99c4afa25a484c02
alemela <alessiom92@gmail.com>
2014-04-11 21:43:35 +0200

    Fix latest query

--- query/itwiki/EGO/Varie/Pagine_del_ns_Aiuto_non_editate_da_piu_tempo.sql
+++ query/itwiki/EGO/Varie/Pagine_del_ns_Aiuto_non_editate_da_piu_tempo.sql
@@ -1,5 +1,5 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT('#[[Wikipedia:', page_title, ']], (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ')')
+SELECT CONCAT('#[[Aiuto:', page_title, ']], (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ')')
   FROM page JOIN revision ON page_latest=rev_id
   WHERE page_namespace=12 AND page_is_redirect=0
     AND page_title NOT LIKE '%/%' 
