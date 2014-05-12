6bd3d1b1c66768140beceac5590b0c9a41a0229f
alemela <alessiom92@gmail.com>
2014-05-12 23:37:30 +0200

    Fixed Pagine create da IP con una sola revisione

--- query/itwiki/EGO/Varie/Pagine_create_da_IP_con_una_sola_revisione.sql
+++ query/itwiki/EGO/Varie/Pagine_create_da_IP_con_una_sola_revisione.sql
@@ -1,8 +1,8 @@
 CONNECT itwiki_p itwiki.labsdb;
 SELECT CONCAT("# [[", ns_name, ":", page_title, "]]")
-FROM page, revision
-JOIN p50380g50614__lists.ns_it ON page_namespace = ns_id
+FROM page, revision, p50380g50614__lists.ns_it
 WHERE page_id = rev_page
+AND page_namespace = ns_id
 AND page_is_new = 1
 AND page_is_redirect = 0
 AND rev_user = 0
