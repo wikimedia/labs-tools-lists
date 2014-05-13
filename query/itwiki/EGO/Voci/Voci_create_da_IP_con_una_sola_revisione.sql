19bd61b7a67406db833d9cb20bd143821792aebe
dmm42 <diegmonti@gmail.com>
2014-05-13 22:59:34 +0200

    Add itwiki/EGO/Voci/Voci_create_da_IP_con_una_sola_revisione

---
+++ query/itwiki/EGO/Voci/Voci_create_da_IP_con_una_sola_revisione.sql
@@ -0,0 +1,9 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT DISTINCT CONCAT("# [[", page_title, "]]")
+FROM page, revision
+WHERE page_id = rev_page
+AND page_is_new = 1
+AND page_is_redirect = 0
+AND rev_user = 0
+AND page_namespace = 0
+ORDER BY page_title;
