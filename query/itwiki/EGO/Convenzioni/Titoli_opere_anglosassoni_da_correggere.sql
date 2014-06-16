8f382717673c9f171e1d0ccb57e59e07e16f9c06
dmm42 <diegmonti@gmail.com>
2014-06-16 16:57:41 +0200

    Update query of itwiki/EGO/Convenzioni/Titoli_opere_anglosassoni_da_correggere

--- query/itwiki/EGO/Convenzioni/Titoli_opere_anglosassoni_da_correggere.sql
+++ query/itwiki/EGO/Convenzioni/Titoli_opere_anglosassoni_da_correggere.sql
@@ -2,4 +2,4 @@ CONNECT itwiki_p itwiki.labsdb;
 SELECT CONCAT("# [[", page_title, "]]") 
   FROM page 
   WHERE page_namespace = 0 
-  AND page_title REGEXP '[^\.:\-\?\!]_(A|An|The|And|But|Nor|Or|As|At|By|For|From|In|Of|On|Over|To|With)_.*'
+  AND page_title REGEXP '[^-:\\.\\!\\?\\–]_(A|An|The|And|But|Nor|Or|As|At|By|For|From|In|Of|On|Over|To|With)_'
