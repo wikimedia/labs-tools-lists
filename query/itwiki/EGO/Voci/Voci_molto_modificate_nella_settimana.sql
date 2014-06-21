001e1a583b45d3dc8d6d241fe33e3e9beb7af895
dmm42 <diegmonti@gmail.com>
2014-06-21 22:45:38 +0200

    Add itwiki/EGO/Voci/Voci_molto_modificate_nella_settimana

---
+++ query/itwiki/EGO/Voci/Voci_molto_modificate_nella_settimana.sql
@@ -0,0 +1,10 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT("# [[", page_title, "]]"), COUNT(rev_id)
+FROM page
+JOIN revision ON page_id = rev_page
+WHERE page_is_redirect = 0
+AND page_namespace = 0
+AND rev_timestamp > NOW() - INTERVAL 1 WEEK
+GROUP BY page_title
+ORDER BY COUNT(rev_id) DESC
+LIMIT 100;
