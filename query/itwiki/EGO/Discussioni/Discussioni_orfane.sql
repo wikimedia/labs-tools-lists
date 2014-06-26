9f9d0ae0e587d552dd6edd59c80038e814ade599
dmm42 <diegmonti@gmail.com>
2014-06-26 20:27:43 +0200

    Fix itwiki/EGO/Discussioni/Discussioni_orfane

--- query/itwiki/EGO/Discussioni/Discussioni_orfane.sql
+++ query/itwiki/EGO/Discussioni/Discussioni_orfane.sql
@@ -1,9 +1,10 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT('# [[', nstalk.ns_name, ':', disc.page_title, ']] ([[Speciale:PuntanoQui/', nstalk.ns_name, ':', disc.page_title, ']] - [[Special:Undelete/', nspage.ns_name, ':', disc.page_title, ']] - ', disc.page_len, ' byte)')
-  FROM page as disc
+SELECT CONCAT('# [[', nstalk.ns_name, ':', disc.page_title, ']] (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ')')
+  FROM page AS disc
   JOIN p50380g50614__lists.ns_it AS nstalk ON disc.page_namespace = nstalk.ns_id
   JOIN p50380g50614__lists.ns_it AS nspage ON (disc.page_namespace - 1) = nspage.ns_id
-  WHERE disc.page_namespace in (1,5,7,9,11,13,15,101,103)
+  JOIN revision ON disc.page_latest = rev_id
+  WHERE disc.page_namespace IN (1,5,7,9,11,13,15,101,103)
     AND disc.page_title NOT LIKE '%/da_fare'
     AND disc.page_title NOT REGEXP './[Aa]rchiv.'
     AND NOT EXISTS (
@@ -11,4 +12,4 @@ SELECT CONCAT('# [[', nstalk.ns_name, ':', disc.page_title, ']] ([[Speciale:Punt
       FROM page
       WHERE page_title = disc.page_title
 			AND page_namespace = disc.page_namespace - 1)
-  ORDER BY nstalk.ns_name, disc.page_title;
+  ORDER BY nstalk.ns_name, rev_timestamp, disc.page_title;
--- query/itwiki/EGO/Discussioni/Discussioni_orfane_di_redirect.sql
+++ query/itwiki/EGO/Discussioni/Discussioni_orfane_di_redirect.sql
@@ -1,12 +1,13 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT('# [[', nstalk.ns_name, ':', page.page_title, ']] ([[Speciale:PuntanoQui/', nstalk.ns_name, ':', page.page_title, ']]) -> [[', nspage.ns_name, ':', rd_title, ']] ([[', nstalk.ns_name, ':', rd_title, ']])')
+SELECT CONCAT('# [[', nstalk.ns_name, ':', page.page_title, ']] (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ')')
 FROM redirect
 JOIN page ON rd_from = page_id
 INNER JOIN page AS disc ON page.page_title = disc.page_title
+JOIN revision ON disc.page_latest = rev_id
 JOIN p50380g50614__lists.ns_it AS nstalk ON disc.page_namespace = nstalk.ns_id
 JOIN p50380g50614__lists.ns_it AS nspage ON (disc.page_namespace - 1) = nspage.ns_id
   WHERE page.page_namespace = 0
     AND disc.page_namespace = 1
     AND page.page_is_redirect = 1
     AND disc.page_is_redirect = 0
-ORDER BY page.page_title;
+ORDER BY nstalk.ns_name, rev_timestamp, page.page_title;
