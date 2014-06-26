2ce9ac6011afc084fe7f7169c3b1baee7351f753
dmm42 <diegmonti@gmail.com>
2014-06-26 19:16:38 +0200

    Fix itwiki/EGO/Template/Manuali_dei_template

--- query/itwiki/EGO/Template/Manuali_dei_template.sql
+++ query/itwiki/EGO/Template/Manuali_dei_template.sql
@@ -1,13 +1,14 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT("# [[Template:", page_title, "]]")  
-  FROM (SELECT REPLACE(page_title, '/man', '') AS page_title 
-    FROM page 
-    WHERE page_namespace = 10 
+SELECT CONCAT("# [[Template:", page_title, "]]")
+  FROM (SELECT REPLACE(page_title, '/man', '') AS page_title
+    FROM page
+    WHERE page_namespace = 10
+      AND page_is_redirect = 0
       AND page_title LIKE '%/man') AS t1
-  WHERE page_title NOT IN (SELECT page_title 
-    FROM (SELECT p2.page_id AS page_id, p2.page_title AS page_title 
-      FROM page AS p1, page AS p2 
-      WHERE p1.page_title = CONCAT(p2.page_title, "/man") 
-        AND p1.page_namespace = 10 
-        AND p1.page_namespace = p2.page_namespace) AS p 
+  WHERE page_title NOT IN (SELECT page_title
+    FROM (SELECT p2.page_id AS page_id, p2.page_title AS page_title
+      FROM page AS p1, page AS p2
+      WHERE p1.page_title = CONCAT(p2.page_title, "/man")
+        AND p1.page_namespace = 10
+        AND p1.page_namespace = p2.page_namespace) AS p
     JOIN templatelinks ON page_id = tl_from WHERE tl_title = "Man");
