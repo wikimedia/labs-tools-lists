8c3999ed03529b8bb3eb3cd194c39df498b6fad3
dmm42 <diegmonti@gmail.com>
2014-06-27 21:43:33 +0200

    General renaming and remove unused lists

--- query/itwiki/EGO/Immagini/Immagini_con_stesso_nome_su_Commons.sql
+++
@@ -1,8 +0,0 @@
-CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT("* [[:File:", itwiki_p.image.img_name, "|]] (", itwiki_p.image.img_size,
-       "; su [[:commons:File:", itwiki_p.image.img_name, "|commons]]: ", 
-       commonswiki_f_p.image.img_size, ")")
-FROM image
-LEFT JOIN commonswiki_f_p.image
-ON commonswiki_f_p.image.img_name = itwiki_p.image.img_name
-ORDER BY itwiki_p.image.img_name;
