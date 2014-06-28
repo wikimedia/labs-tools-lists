2735f605746f7ad971533d763e7b4544c1014187
dmm42 <diegmonti@gmail.com>
2014-06-28 22:50:18 +0200

    Add itwiki/Template/Canzoni_contenenti_il_template_album

---
+++ query/itwiki/Template/Canzoni_contenenti_il_template_album.sql
@@ -0,0 +1,11 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT DISTINCT CONCAT("# [[", page_title, "]]")
+  FROM page JOIN categorylinks ON page_id = cl_from
+  WHERE
+    page_namespace=0
+      AND page_is_redirect=0
+      AND cl_to LIKE 'Brani_musicali%'
+      AND page_id IN (SELECT tl_from
+                      FROM templatelinks
+                      WHERE tl_namespace=10 AND tl_title='Album')
+  ORDER BY page_title;
