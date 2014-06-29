950fbc019f36fcf3189f7bb3e602c8bfd326afe3
dmm42 <diegmonti@gmail.com>
2014-06-29 22:39:24 +0200

    Graphic fixes

--- query/itwiki/Pagine/Pagine_del_ns_Aiuto_in_ordine_di_ultimo_edit.sql
+++ query/itwiki/Pagine/Pagine_del_ns_Aiuto_in_ordine_di_ultimo_edit.sql
@@ -1,7 +1,7 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT('#[[Aiuto:', page_title, ']], (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ')')
+SELECT CONCAT('# [[Aiuto:', page_title, ']], (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ')')
   FROM page JOIN revision ON page_latest=rev_id
   WHERE page_namespace=12 AND page_is_redirect=0
-    AND page_title NOT LIKE '%/%' 
+    AND page_title NOT LIKE '%/%'
   ORDER BY rev_timestamp ASC
   LIMIT 5000;
