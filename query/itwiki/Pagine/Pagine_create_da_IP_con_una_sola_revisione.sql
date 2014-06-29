457c59f1740cd6775f3fa2b9b0675889a60158c3
alemela <alessiom92@gmail.com>
2014-06-29 22:41:51 +0200

    Limit results number of Pagine create da IP con una sola rev

--- query/itwiki/Pagine/Pagine_create_da_IP_con_una_sola_revisione.sql
+++ query/itwiki/Pagine/Pagine_create_da_IP_con_una_sola_revisione.sql
@@ -1,5 +1,5 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT DISTINCT CONCAT("# [[", ns_name, ":", page_title, "]]")
+SELECT DISTINCT CONCAT("# [[", ns_name, ":", page_title, "]], (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ')")
 FROM page, revision, p50380g50614__lists.ns_it
 WHERE page_id = rev_page
 AND page_namespace = ns_id
@@ -7,4 +7,5 @@ AND page_is_new = 1
 AND page_is_redirect = 0
 AND rev_user = 0
 AND page_namespace <> 0
-ORDER BY page_namespace, page_title;
+ORDER BY rev_timestamp, page_namespace, page_title
+LIMIT 5000;
