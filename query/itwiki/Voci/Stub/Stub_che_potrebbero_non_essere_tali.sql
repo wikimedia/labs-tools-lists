90c4990ecec47a9c772095a9562655e5e391a9ad
dmm42 <diegmonti@gmail.com>
2014-06-29 22:38:46 +0200

    Add some limits

--- query/itwiki/Voci/Stub/Stub_che_potrebbero_non_essere_tali.sql
+++ query/itwiki/Voci/Stub/Stub_che_potrebbero_non_essere_tali.sql
@@ -5,5 +5,5 @@ JOIN templatelinks ON page_id = tl_from
 WHERE page_namespace = 0
 AND tl_title = 'S'
 AND tl_namespace = 10
-AND page_len > 5000
+AND page_len > 10000
 ORDER BY page_len DESC, page_title;
