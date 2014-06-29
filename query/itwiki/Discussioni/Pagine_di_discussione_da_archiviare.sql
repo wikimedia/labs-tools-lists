b0bb7364f323329c4f419cf2d409db4bfc0ae50f
dmm42 <diegmonti@gmail.com>
2014-06-29 22:18:50 +0200

    Add Discussioni/Pagine_di_discussione_da_archiviare

---
+++ query/itwiki/Discussioni/Pagine_di_discussione_da_archiviare.sql
@@ -0,0 +1,8 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT("# [[Discussione:", page_title, "]]")
+FROM page
+WHERE page_namespace = 1
+AND page_is_redirect = 0
+AND page_len > 100000
+AND page_title NOT LIKE '%/%'
+ORDER BY page_title;
