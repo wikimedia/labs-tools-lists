1b6fe4e0c6df2fbfe5062433bc632eb7e05cc2b1
alemela <alessiom92@gmail.com>
2014-06-29 22:50:46 +0200

    Add query Voci su biografie di viventi con info senza fonti

---
+++ query/itwiki/Voci/Voci_su_biografie_di_viventi_con_info_senza_fonti.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT('# [[', page_title, ']]')
+    FROM page JOIN templatelinks ON page_id = tl_from
+              JOIN categorylinks ON page_id = cl_from
+    WHERE page_namespace = 0
+      AND ( tl_title = 'Citazione_necessaria'
+         OR tl_title = 'F' )
+      AND tl_namespace = 10
+      AND cl_to = 'Persone_viventi'
+    ORDER BY page_title;
