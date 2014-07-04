16c6751e7953a524d1e757f3afc16bf48f87b584
dmm42 <diegmonti@gmail.com>
2014-07-04 22:08:07 +0200

    Add itwiki/Disambigue/Disambigue_per_numero_di_collegamenti_in_ingresso

---
+++ query/itwiki/Disambigue/Disambigue_per_numero_di_collegamenti_in_ingresso.sql
@@ -0,0 +1,18 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', page_title, ']] (', COUNT(*), ')')
+FROM page, pagelinks
+WHERE pl_title = page_title
+AND page_namespace = 0
+AND page_is_redirect = 0
+AND pl_namespace = 0
+AND page_id IN
+(SELECT pp_page
+FROM page_props
+WHERE pp_propname = 'disambiguation')
+AND pl_from IN
+(SELECT page_id
+FROM page
+WHERE page_namespace = 0)
+GROUP BY page_title
+ORDER BY COUNT(*) DESC
+LIMIT 5000;
