b924c637bfe9419d05a1226b2438d9ccf4bc1456
dmm42 <diegmonti@gmail.com>
2014-07-04 22:10:50 +0200

    Add itwiki/Voci/Voci_orfane_per_data_di_creazione

---
+++ query/itwiki/Voci/Voci_orfane_per_data_di_creazione.sql
@@ -0,0 +1,32 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', page_title, ']]')
+FROM page, revision
+WHERE page_namespace = 0
+AND page_is_redirect = 0
+AND rev_page = page_id
+AND rev_parent_id = 0
+AND page_title NOT IN
+(SELECT pl_title
+FROM pagelinks
+WHERE pl_namespace = 0
+AND pl_from NOT IN
+(SELECT page_id
+FROM page
+WHERE page_namespace <> 0)
+AND pl_from NOT IN
+(SELECT cl_from
+FROM categorylinks, pagelinks
+WHERE cl_to = pl_title
+AND pl_namespace = 14
+AND pl_from = 4195411)) /* Wikipedia:ConnectivityProjectInternationalization/ArticlesNotFormingValidLinks */
+AND page_id NOT IN
+(SELECT pp_page
+FROM page_props
+WHERE pp_propname = 'disambiguation')
+AND page_id NOT IN
+(SELECT cl_from
+FROM categorylinks, pagelinks
+WHERE cl_to = pl_title
+AND pl_namespace = 14
+AND pl_from = 4195411) /* Wikipedia:ConnectivityProjectInternationalization/ArticlesNotFormingValidLinks */
+ORDER BY rev_timestamp;
