13820ae27cd9ed0b31041c340ff703b125151073
dmm42 <diegmonti@gmail.com>
2014-07-08 18:45:20 +0200

    Fix itwiki/Redirect/Redirect_a_sezioni

--- query/itwiki/Redirect/Redirect_a_sezioni.sql
+++ query/itwiki/Redirect/Redirect_a_sezioni.sql
@@ -1,8 +1,8 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT("# [[", page_title, "]], (", DATE_FORMAT(page_latest, '%e %b %Y, %H:%i'), ")")
+SELECT CONCAT("# [[", page_title, "]], (", DATE_FORMAT(page_touched, '%e %b %Y, %H:%i'), ")")
 FROM page, redirect
 WHERE rd_from = page_id
 AND page_namespace = 0
 AND rd_fragment NOT LIKE ''
-ORDER BY page_latest DESC
+ORDER BY page_touched DESC
 LIMIT 1000;
