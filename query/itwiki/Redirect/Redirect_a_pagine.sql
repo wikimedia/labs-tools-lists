c3eb5860b8705ff25a80ab59a78d580bc2d286d3
dmm42 <diegmonti@gmail.com>
2014-07-09 18:46:46 +0200

    Remove double query

--- query/itwiki/Redirect/Redirect_a_pagine.sql
+++
@@ -1,7 +0,0 @@
-CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT('# [[', page_title, ']]')
-FROM page, redirect
-WHERE rd_from = page_id
-AND page_namespace = 0
-AND rd_namespace <> 0
-ORDER BY page_title;
