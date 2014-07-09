2164e125b87c18c92491b8bec71017f8a535660d
dmm42 <diegmonti@gmail.com>
2014-07-09 22:37:31 +0200

    Fix itwiki/Template/Biografie_senza_template_Bio

--- query/itwiki/Template/Biografie_senza_template_Bio.sql
+++ query/itwiki/Template/Biografie_senza_template_Bio.sql
@@ -10,4 +10,11 @@ AND page_id NOT IN
 (SELECT page_id
 FROM page JOIN templatelinks ON page_id = tl_from
 WHERE tl_namespace = 10 AND tl_title = 'Bio')
+AND page_title NOT IN
+(SELECT page_title
+FROM categorylinks, pagelinks, page
+WHERE cl_to = pl_title
+AND cl_from = page_id
+AND pl_namespace = 14
+AND pl_from = 4195411) /* Wikipedia:ConnectivityProjectInternationalization/ArticlesNotFormingValidLinks */
 ORDER BY page_title;
