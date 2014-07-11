8fb9ca08b2bcf32d3e8835c56820ef989c54dccc
dmm42 <diegmonti@gmail.com>
2014-07-11 23:14:47 +0200

    Fix itwiki/Voci/Voci_poco_modificate_per_data_di_creazione

--- query/itwiki/Voci/Voci_poco_modificate_per_data_di_creazione.sql
+++ query/itwiki/Voci/Voci_poco_modificate_per_data_di_creazione.sql
@@ -1,5 +1,5 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT("# [[", page_title, "]], (", DATE_FORMAT(page_touched, '%e %b %Y, %H:%i'), ")")
+SELECT CONCAT("# [[", page_title, "]], (", DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ")")
 FROM page
 JOIN revision ON page_id = rev_page
 LEFT JOIN langlinks ON ll_from = rev_page
@@ -10,5 +10,5 @@ AND ll_from IS NULL
 AND rev_timestamp < NOW() - INTERVAL 6 YEAR
 AND rev_len < 5000
 AND CAST(page_len AS SIGNED)-CAST(rev_len AS SIGNED) < 500
-ORDER BY page_touched DESC, page_title
+ORDER BY rev_timestamp, page_title
 LIMIT 10000;
