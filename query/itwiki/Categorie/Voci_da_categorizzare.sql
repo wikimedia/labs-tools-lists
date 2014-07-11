670aa7acce77066ecf2fabd0fb3c0546fc441145
dmm42 <diegmonti@gmail.com>
2014-07-11 17:42:06 +0200

    Fix itwiki/Categorie/Voci_da_categorizzare

--- query/itwiki/Categorie/Voci_da_categorizzare.sql
+++ query/itwiki/Categorie/Voci_da_categorizzare.sql
@@ -1,13 +1,19 @@
 CONNECT itwiki_p itwiki.labsdb;
 SELECT CONCAT("# [[", page_title, "]]")
 FROM page
-WHERE page_id NOT IN (
-SELECT cl_from
-FROM categorylinks
-WHERE cl_from NOT IN (
-SELECT cl_from
-FROM categorylinks
-WHERE cl_to = 'Categorie_nascoste'))
-AND page_namespace = 0
+WHERE page_namespace = 0
 AND page_is_redirect = 0
+AND page_id NOT IN
+(SELECT page_id
+FROM page
+WHERE page_id IN (
+SELECT cl_from
+FROM categorylinks
+WHERE cl_to NOT IN (
+SELECT page_title
+FROM categorylinks, page
+WHERE cl_to = 'Categorie_nascoste'
+AND page_id = cl_from))
+AND page_namespace = 0
+AND page_is_redirect = 0)
 ORDER BY page_title;
