1717bb3ac1945fe8acdbe617dc06a368a4c45443
dmm42 <diegmonti@gmail.com>
2014-07-12 22:23:05 +0200

    Add itwiki/Varie/Collegamenti_esterni_presenti_con_maggiore_frequenza

---
+++ query/itwiki/Varie/Collegamenti_esterni_presenti_con_maggiore_frequenza.sql
@@ -0,0 +1,10 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT( "# ", nome, " (", numero, ")")
+FROM (
+SELECT el_to AS nome, count( * ) AS numero
+FROM externallinks
+GROUP BY el_to
+) AS link
+WHERE numero >= 100
+AND nome NOT REGEXP ".*wiki(pedia|media)\.org.*"
+ORDER BY numero DESC;
