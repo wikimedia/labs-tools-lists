f00356c46cd98b27d5e2380f750e399e0416dfef
dmm42 <diegmonti@gmail.com>
2014-07-20 20:46:09 +0200

    Add itwiki/File/File_su_Commons_con_pagina_locale

---
+++ query/itwiki/File/File_su_Commons_con_pagina_locale.sql
@@ -0,0 +1,9 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[:File:', page_title, ']]')
+FROM page
+WHERE page_namespace = 6
+AND page_is_redirect = 0
+AND page_title NOT IN
+(SELECT img_name
+FROM image)
+ORDER BY page_title;
