ef993a93cf7e64997b171d7e483e60435653de7d
alemela <alessiom92@gmail.com>
2014-07-20 17:46:24 +0200

    Add query/Redirect_categorizzati

---
+++ query/itwiki/Redirect/Redirect_categorizzati.sql
@@ -0,0 +1,9 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', page_title, ']] in [[Categoria:', cl_to, ']]')
+FROM page, categorylinks
+WHERE page_is_redirect = 1
+AND page_id = cl_from
+AND cl_to <> 'Redirect_da_mantenere_orfani'
+AND cl_to <> 'Formule_molecolari'
+GROUP BY page_title
+ORDER BY page_title;
