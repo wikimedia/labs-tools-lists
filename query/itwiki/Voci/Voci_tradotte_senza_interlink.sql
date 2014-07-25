41551c025183f1e55bc04278bc35c84b0f2cc717
alemela <alessiom92@gmail.com>
2014-07-26 01:35:48 +0200

    Add query/Voci_tradotte_senza_interlink

---
+++ query/itwiki/Voci/Voci_tradotte_senza_interlink.sql
@@ -0,0 +1,13 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', p2.page_title, ']]')
+FROM page AS p1 JOIN templatelinks ON p1.page_id = tl_from, page AS p2
+WHERE p1.page_namespace = 1
+AND p1.page_is_redirect = 0
+AND tl_title = 'Tradotto_da'
+AND p1.page_title = p2.page_title
+AND p2.page_namespace = 0
+AND p2.page_is_redirect = 0
+AND p2.page_id NOT IN
+(SELECT ll_from
+FROM langlinks)
+ORDER BY p2.page_title;
