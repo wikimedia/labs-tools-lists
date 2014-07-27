99d750d6933b4af498409f56efcdeabb2aa2ccd5
alemela <alessiom92@gmail.com>
2014-07-27 17:13:02 +0200

    Add query/Voci_orfane_in_ordine_alfabetico

---
+++ query/itwiki/Voci/Voci_orfane_in_ordine_alfabetico.sql
@@ -0,0 +1,30 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', page_title, ']]')
+FROM page
+WHERE page_namespace = 0
+AND page_is_redirect = 0
+AND page_title NOT IN
+(SELECT pl_title
+FROM pagelinks
+WHERE pl_namespace = 0
+AND pl_from NOT IN
+(SELECT page_id
+FROM page
+WHERE page_namespace <> 0)
+AND pl_from NOT IN
+(SELECT cl_from
+FROM categorylinks, pagelinks
+WHERE cl_to = pl_title
+AND pl_namespace = 14
+AND pl_from = 4195411)) /* Wikipedia:ConnectivityProjectInternationalization/ArticlesNotFormingValidLinks */
+AND page_id NOT IN
+(SELECT pp_page
+FROM page_props
+WHERE pp_propname = 'disambiguation')
+AND page_id NOT IN
+(SELECT cl_from
+FROM categorylinks, pagelinks
+WHERE cl_to = pl_title
+AND pl_namespace = 14
+AND pl_from = 4195411) /* Wikipedia:ConnectivityProjectInternationalization/ArticlesNotFormingValidLinks */
+ORDER BY page_title;
