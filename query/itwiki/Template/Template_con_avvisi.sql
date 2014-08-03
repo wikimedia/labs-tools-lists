b480f0b12ce507c5dbf7e4c4a8da70323810c19b
alemela <alessiom92@gmail.com>
2014-08-03 12:00:39 +0200

    Add query/Template_con_avvisi

---
+++ query/itwiki/Template/Template_con_avvisi.sql
@@ -0,0 +1,21 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT("# [[Template:", page_title, "]]")
+FROM page, templatelinks
+WHERE page_id = tl_from
+AND tl_namespace = 10
+AND page_namespace = 10
+AND tl_title IN ('S', 'Stub', 'W', 'PW', 'Wikificare','A', 'C',
+                       'Da_controllare', 'E', 'T', 'T_sezione', 'P', 'V',
+                       'Bufala', 'L', 'Localismo', 'F', 'Senza_fonti',
+                       'Senzafonti','U', 'Merge', 'Da_unire', 'Correggere',
+                       'Da_correggere', 'Errori_grammaticali', 'NN', 'N',
+                       'Controlcopy', 'Voci_senza_uscita', 'Categorizzare',
+                       'Tmp', 'Manca_template', 'Tradurre', 'O', 'Dividi',
+                       'Chiarire', 'Non_chiaro', 'Da_chiarire', 'D',
+                       'Recentismo', 'Organizzare', 'Finzione', 'Fact',
+                       'Citazione_necessaria', 'Senza fonte', 'CN', 'Sf',
+                       'Cn', 'Citation_needed', 'S_sezione', 'Da_aggiornare')
+AND tl_title <> page_title
+AND page_title NOT LIKE '%/%'
+GROUP BY page_id
+ORDER BY page_title;
