f564de4a4950589ffe15c97021511156d4f41bd3
alemela <alessiom92@gmail.com>
2014-08-03 12:02:42 +0200

    Update query/Immagini_orfane

--- query/itwiki/File/Immagini/Immagini_orfane.sql
+++ query/itwiki/File/Immagini/Immagini_orfane.sql
@@ -1,9 +1,12 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT concat("# [[:Immagine:", page.page_title, "]]")
-  FROM page JOIN templatelinks ON page.page_id = templatelinks.tl_from
-  WHERE templatelinks.tl_title = 'EDP'
-    AND page.page_namespace = 6
-    AND page.page_title NOT IN (
-      SELECT DISTINCT imagelinks.il_to FROM imagelinks
-    )
-  ORDER BY page.page_title;
+SELECT CONCAT('# [[:File:', page_title, ']]')
+FROM page
+WHERE page_namespace = 6
+AND page_is_redirect = 0
+AND page_title NOT IN
+(SELECT il_to
+FROM imagelinks)
+AND page_title IN
+(SELECT img_name
+FROM image)
+ORDER BY page_title;
