e6859e16e29b573e8dbfdab5f035337c6adeaff3
dmm42 <diegmonti@gmail.com>
2014-08-14 14:35:22 +0200

    According to Labs Terms of use

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -45,16 +45,6 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-38831639-2']);
-        _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
 
   </body>
 </html>
