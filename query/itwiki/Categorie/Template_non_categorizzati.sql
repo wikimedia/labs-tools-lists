4b14d86517a1f16ea8531b94695130b3159b75ce
alemela <alessiom92@gmail.com>
2014-08-21 13:54:06 +0200

    Fix query/Template_non_categorizzati

--- query/itwiki/Categorie/Template_non_categorizzati.sql
+++ query/itwiki/Categorie/Template_non_categorizzati.sql
@@ -1,10 +1,20 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT('# [[Template:', page_title, ']]')
+SELECT CONCAT("# [[Template:", page_title, "]]")
 FROM page
 WHERE page_namespace = 10
-AND page_is_redirect = 0
 AND page_title NOT LIKE '%/%'
+AND page_is_redirect = 0
 AND page_id NOT IN
-(SELECT cl_from
-FROM categorylinks)
+(SELECT page_id
+FROM page
+WHERE page_id IN (
+SELECT cl_from
+FROM categorylinks
+WHERE cl_to NOT IN (
+SELECT page_title
+FROM categorylinks, page
+WHERE cl_to = 'Categorie_nascoste'
+AND page_id = cl_from))
+AND page_namespace = 10
+AND page_is_redirect = 0)
 ORDER BY page_title;
