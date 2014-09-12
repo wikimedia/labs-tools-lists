f5f97ad6b3317a9a32d3d30c2f8b3c48a8caff67
dmm42 <diegmonti@gmail.com>
2014-09-12 14:37:14 +0200

    Update itwiki/Template/Template_non_utilizzati

--- query/itwiki/Template/Template_non_utilizzati.sql
+++ query/itwiki/Template/Template_non_utilizzati.sql
@@ -8,4 +8,10 @@ WHERE tl_namespace = 10)
 AND page_namespace = 10
 AND page_is_redirect = 0
 AND page_title NOT LIKE '%/%'
+AND page_id NOT IN (
+SELECT tl_from
+FROM templatelinks
+WHERE tl_from_namespace = 10
+AND tl_namespace = 10
+AND tl_title = 'Template_non_incluso')
 ORDER BY page_title;
