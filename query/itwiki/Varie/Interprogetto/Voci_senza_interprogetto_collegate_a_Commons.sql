fd8aced527b3c479516585b9cad80b691dc9e528
dmm42 <diegmonti@gmail.com>
2014-09-15 21:58:17 +0200

    Add query/itwiki/Varie/Interprogetto

---
+++ query/itwiki/Varie/Interprogetto/Voci_senza_interprogetto_collegate_a_Commons.sql
@@ -0,0 +1,27 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT DISTINCT CONCAT('# [[', page_title, ']]')
+FROM page
+WHERE page_namespace = 0
+AND page_is_redirect = 0
+AND page_id NOT IN (
+SELECT tl_from
+FROM templatelinks
+WHERE tl_namespace = 10
+AND tl_title IN (
+'Interprogetto',
+'Ip',
+'Commons',
+'Commons_cat',
+'Commons_category',
+'Commons_cat_left',
+'Commonscat',
+'Commonscat-inline',
+'Commonscat_left',
+'Disambigua' ) )
+AND page_title IN (
+SELECT REPLACE(WIKI.ips_site_page, ' ', '_')
+FROM wikidatawiki_p.wb_items_per_site WIKI, wikidatawiki_p.wb_items_per_site EXT
+WHERE WIKI.ips_item_id = EXT.ips_item_id
+AND WIKI.ips_site_id = 'itwiki'
+AND EXT.ips_site_id = 'commonswiki' )
+ORDER BY page_title;
---
+++ query/itwiki/Varie/Interprogetto/Voci_senza_interprogetto_collegate_a_Wikivoyage.sql
@@ -0,0 +1,20 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT DISTINCT CONCAT('# [[', page_title, ']]')
+FROM page
+WHERE page_namespace = 0
+AND page_is_redirect = 0
+AND page_id NOT IN (
+SELECT tl_from
+FROM templatelinks
+WHERE tl_namespace = 10
+AND tl_title IN (
+'Interprogetto',
+'Ip',
+'Disambigua' ) )
+AND page_title IN (
+SELECT REPLACE(WIKI.ips_site_page, ' ', '_')
+FROM wikidatawiki_p.wb_items_per_site WIKI, wikidatawiki_p.wb_items_per_site EXT
+WHERE WIKI.ips_item_id = EXT.ips_item_id
+AND WIKI.ips_site_id = 'itwiki'
+AND EXT.ips_site_id = 'itwikivoyage' )
+ORDER BY page_title;
