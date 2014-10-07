14c2acc342834bc17c015c12865d0b7a6d290226
alemela <alessiom92@gmail.com>
2014-10-07 23:22:23 +0200

    Add query/Disambigue_con_immagini

---
+++ query/itwiki/Disambigue/Disambigue_con_immagini.sql
@@ -0,0 +1,15 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', page_title, ']]')
+FROM page
+WHERE page_namespace = 0
+AND page_is_redirect = 0
+AND page_id IN
+(SELECT il_from
+FROM imagelinks
+WHERE il_to <> 'Disambigua_compass.svg'
+AND il_to <> 'Wiktionary_small.svg'
+AND il_to <> 'Nuvola_mimetypes_charnotfound.PNG')
+AND page_id IN 
+(SELECT pp_page
+FROM page_props
+WHERE pp_propname = 'disambiguation')
