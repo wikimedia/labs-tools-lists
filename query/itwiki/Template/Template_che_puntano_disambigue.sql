76b35aba9da50908e17d3de0a282e6c7d7991eca
dmm42 <diegmonti@gmail.com>
2014-11-13 21:51:57 +0100

    Update Template/Template_che_puntano_disambigue

--- query/itwiki/Template/Template_che_puntano_disambigue.sql
+++ query/itwiki/Template/Template_che_puntano_disambigue.sql
@@ -1,14 +1,29 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT CONCAT('# [[Template:', p1.page_title, ']] punta alla disambigua [[', p2.page_title, ']]')
-  FROM page AS p1
-    JOIN pagelinks
-    ON p1.page_id = pl_from
-    JOIN page AS p2 ON pl_title = p2.page_title AND pl_namespace = p2.page_namespace
-    JOIN templatelinks ON p2.page_id = tl_from
-  WHERE p1.page_namespace = 10
-    AND p2.page_namespace = 0
-    AND p1.page_is_redirect = 0
-    AND p2.page_is_redirect = 0
-    AND p1.page_title NOT LIKE '%/man'
-    AND tl_title IN ( 'Disambigua', 'Sigla2', 'Sigla3' )
-  ORDER BY p1.page_title;
+SELECT CONCAT('# [[Template:', page_title, ']] -> [[', pl_title, ']]')
+FROM pagelinks,
+     page
+WHERE page_namespace = 10
+  AND page_id = pl_from
+  AND pl_namespace = 0
+  AND pl_title IN
+    (SELECT page_title
+     FROM pagelinks,
+          page
+     WHERE page_namespace = 0
+       AND page_id = pl_from
+       AND page_is_redirect = 1
+       AND pl_title IN
+         (SELECT page_title
+          FROM page_props,
+               page
+          WHERE pp_page = page_id
+            AND page_namespace = 0
+            AND pp_propname = 'disambiguation')
+     UNION SELECT page_title
+     FROM page_props,
+          page
+     WHERE pp_page = page_id
+       AND page_namespace = 0
+       AND pp_propname = 'disambiguation')
+  AND page_title NOT LIKE '%/man'
+ORDER BY page_title;
