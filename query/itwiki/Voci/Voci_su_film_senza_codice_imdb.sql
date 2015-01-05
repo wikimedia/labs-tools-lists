8512dbbd5601f31243868555afeb9a38ee18aeeb
dmm42 <diegmonti@gmail.com>
2015-01-05 16:41:13 +0100

    Update itwiki/Voci/Voci_su_film_senza_codice_imdb

--- query/itwiki/Voci/Voci_su_film_senza_codice_imdb.sql
+++ query/itwiki/Voci/Voci_su_film_senza_codice_imdb.sql
@@ -10,5 +10,8 @@ WHERE tl_title = 'Film')
 AND page_id NOT IN
 (SELECT tl_from
 FROM templatelinks
-WHERE tl_title = 'Imdb')
+WHERE tl_title = 'Imdb'
+OR tl_title = 'IMDB'
+OR tl_title = 'IMDb'
+OR tl_title = 'Collegamenti_cinema')
 ORDER BY page_title;
