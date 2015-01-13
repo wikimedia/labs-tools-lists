e2bf43b8af0f8d5dbc045c8bd78283a04c08ff6b
alemela <alessiom92@gmail.com>
2015-01-13 18:42:41 +0100

    Add 2 itwiki queries

---
+++ query/itwiki/Voci/Voci_senza_interlink_per_creatore.sql
@@ -0,0 +1,13 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[Utente:', rev_user_text, ']] (', COUNT(*), ')')
+FROM page, revision
+LEFT JOIN langlinks ON ll_from = rev_page
+WHERE page_is_redirect = 0
+AND page_namespace = 0
+AND page_id = rev_page
+AND rev_parent_id = 0
+AND ll_from IS NULL
+GROUP BY rev_user_text
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC
+LIMIT 1000;
