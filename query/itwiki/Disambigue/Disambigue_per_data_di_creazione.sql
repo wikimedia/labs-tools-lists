49ddf6368fe24994423249c8aa17a27b19d5be68
alemela <alessiom92@gmail.com>
2015-02-05 15:32:24 +0100

    Add new query

---
+++ query/itwiki/Disambigue/Disambigue_per_data_di_creazione.sql
@@ -0,0 +1,20 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', page_title, ']] (', DATE_FORMAT(rev_timestamp, '%e %b %Y, %H:%i'), ') (', COUNT(*), ')')
+FROM page, pagelinks, revision
+WHERE pl_title = page_title
+AND page_namespace = 0
+AND page_is_redirect = 0
+AND pl_namespace = 0
+AND page_id = rev_page
+AND rev_parent_id = 0
+AND page_id IN
+(SELECT pp_page
+FROM page_props
+WHERE pp_propname = 'disambiguation')
+AND pl_from IN
+(SELECT page_id
+FROM page
+WHERE page_namespace = 0)
+GROUP BY page_title
+ORDER BY rev_timestamp, COUNT(*) DESC
+LIMIT 5000;
