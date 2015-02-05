5348e9004ba0ad64da9c3a19e52298d816b5f3e7
alemela <alessiom92@gmail.com>
2015-02-05 15:44:37 +0100

    Add new query

---
+++ query/itwiki/Disambigue/Disambigue_lunghe.sql
@@ -0,0 +1,10 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT('# [[', page_title, ']] - ', page_len)
+FROM page
+WHERE page_id IN
+(SELECT pp_page
+FROM page_props
+WHERE pp_propname = 'disambiguation')
+AND page_namespace = 0
+ORDER BY page_len DESC
+LIMIT 1000;
