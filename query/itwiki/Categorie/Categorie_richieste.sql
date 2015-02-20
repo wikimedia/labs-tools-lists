627faf41cc76c0860b5b79554dd5368b0e928824
alemela <alessiom92@gmail.com>
2015-02-20 16:44:28 +0100

    Add new query

---
+++ query/itwiki/Categorie/Categorie_richieste.sql
@@ -0,0 +1,9 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT("# [[:Categoria:", cl_to, "]] contiene [[", page_title, "]]")
+FROM categorylinks, page
+WHERE cl_to NOT IN
+(SELECT page_title
+FROM page
+WHERE page_namespace=14)
+AND cl_from = page_id
+ORDER BY cl_to;
