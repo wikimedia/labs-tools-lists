18dc5a7f2502b7cff9e97f210944ce4feeea8386
alemela <alessiom92@gmail.com>
2015-03-18 19:31:28 +0100

    add counter in a query (fix)

--- query/itwiki/Utenti/Utenti_per_numero_di_edit.sql
+++ query/itwiki/Utenti/Utenti_per_numero_di_edit.sql
@@ -1,6 +1,6 @@
 CONNECT itwiki_p itwiki.labsdb;
 SET @counter = 0;
-SELECT @counter := @counter +1,'\t', user_name, user_editcount
+SELECT @counter := @counter +1, user_name, user_editcount
 FROM (SELECT user_name, user_editcount
 FROM user, user_groups
 WHERE user_id = ug_user
