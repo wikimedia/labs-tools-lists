c22c8213b6fa244fe5f9fc2f5d1bc8b88b8cd8af
alemela <alessiom92@gmail.com>
2015-03-18 19:25:23 +0100

    add counter in a query

--- query/itwiki/Utenti/Utenti_per_numero_di_edit.sql
+++ query/itwiki/Utenti/Utenti_per_numero_di_edit.sql
@@ -1,9 +1,11 @@
 CONNECT itwiki_p itwiki.labsdb;
-SELECT user_name, user_editcount
+SET @counter = 0;
+SELECT @counter := @counter +1,'\t', user_name, user_editcount
+FROM (SELECT user_name, user_editcount
 FROM user, user_groups
 WHERE user_id = ug_user
 AND ug_group NOT LIKE "bot"
 AND ug_group NOT LIKE "ipblock%"
-GROUP BY user_name
+GROUP BY user_name) sub
 ORDER BY user_editcount DESC
 LIMIT 1000
