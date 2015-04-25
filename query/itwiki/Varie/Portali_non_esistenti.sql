bb2b0607da73d5c6f7f33dd876c2aeb07dcc1a38
dmm42 <diegmonti@gmail.com>
2015-04-25 12:14:54 +0200

    Add itwiki/Varie/Portali_non_esistenti

---
+++ query/itwiki/Varie/Portali_non_esistenti.sql
@@ -0,0 +1,11 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT("# [[", page_title, "]] -> [[Portale:", pl_title, "]]")
+FROM pagelinks, page
+WHERE pl_from = page_id
+AND pl_namespace = 100
+AND pl_from_namespace = 0
+AND pl_title NOT IN (
+SELECT page_title
+FROM page
+WHERE page_namespace = 100)
+ORDER BY pl_title, page_title;
