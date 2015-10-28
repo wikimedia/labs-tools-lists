3053c64701b47656e14b5040ce3b3cb5400ec7be
Diego Monti <diegmonti@gmail.com>
2015-10-28 15:25:15 +0100

    Add exclusions to user cat lists

--- query/itwiki/Categorie/Pagine_utente_categorizzate.sql
+++ query/itwiki/Categorie/Pagine_utente_categorizzate.sql
@@ -19,5 +19,14 @@ AND cl_to NOT LIKE "Soci_Wikimedia_Italia"
 AND cl_to NOT LIKE "Staff_della_Wikimedia_Foundation"
 AND cl_to NOT LIKE "IP_noti"
 AND cl_to NOT LIKE "Vandali_di_Wikipedia"
-AND cl_to NOT LIKE "%bot%")
+AND cl_to NOT LIKE "%bot%"
+AND cl_to NOT LIKE "Template%"
+AND cl_to NOT LIKE "Modelli_senza_data"
+AND cl_to NOT LIKE "Errori_di_compilazione%"
+AND cl_to NOT LIKE "Pagine_con_errori_nelle_espressioni"
+AND cl_to NOT LIKE "Promemoria"
+AND cl_to NOT LIKE "Sportivi_con_immagine_non_presente_su_Wikidata"
+AND cl_to NOT LIKE "Strumenti_di_manutenzione"
+AND cl_to NOT LIKE "Collaborazioni%"
+AND cl_to NOT LIKE "Immagini%")
 ORDER BY page_title;
