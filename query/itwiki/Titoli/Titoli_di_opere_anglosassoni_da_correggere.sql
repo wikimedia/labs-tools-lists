a912105c7f75803ea42707b63c3a01bb3268b282
Diego Monti <diegmonti@gmail.com>
2015-11-16 20:56:05 +0100

    Update itwiki/Titoli/Titoli_di_opere_anglosassoni_da_correggere

--- query/itwiki/Titoli/Titoli_di_opere_anglosassoni_da_correggere.sql
+++ query/itwiki/Titoli/Titoli_di_opere_anglosassoni_da_correggere.sql
@@ -2,4 +2,5 @@ CONNECT itwiki_p itwiki.labsdb;
 SELECT CONCAT("# [[", page_title, "]]") 
   FROM page 
   WHERE page_namespace = 0 
-  AND page_title REGEXP '[^-:\\.\\!\\?\\–]_(A|An|The|And|But|Nor|Or|As|At|By|For|From|In|Of|On|Over|To|With)_'
+  AND page_title REGEXP '[^-:\\.\\!\\?\\–\\(\\)]_(A|An|The|And|But|Nor|Or|As|At|By|For|From|In|Of|On|Over|To|With)_'
+  AND page_title NOT REGEXP '^(Divisione|Episodi|Lega|Serie)_'
