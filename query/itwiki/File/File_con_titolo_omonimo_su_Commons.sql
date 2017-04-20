7217d9267e4ef3d36cec36429f57026049fbef8b
Mess <mesand16@gmail.com>
2017-04-20 23:19:49 +0000

    Add new file list "File con titolo omonimo su Commons"
    Change-Id: I99668351ffed1941b7e8509c0fea08b2218eb843

---
+++ query/itwiki/File/File_con_titolo_omonimo_su_Commons.sql
@@ -0,0 +1,4 @@
+CONNECT itwiki_p itwiki.labsdb;
+SELECT CONCAT( "# [[:File:", img1.img_name, "]] - [[:commons:File:", img2.img_name, "]]" )
+FROM image AS img1
+JOIN commonswiki_p.image AS img2 ON img1.img_name = img2.img_name;
