4c7032add614e985149d3eff92b61b99a13ea7d6
alemela <alessiom92@gmail.com>
2017-09-26 15:31:25 +0200

    Increased interval due to false positives

--- query/itwiki/Voci/Voci_create_da_tre_giorni_senza_interlink.sql
+++ query/itwiki/Voci/Voci_create_da_tre_giorni_senza_interlink.sql
@@ -6,6 +6,6 @@ WHERE page_is_redirect = 0
 AND page_namespace = 0
 AND page_id = rev_page
 AND rev_parent_id = 0
-AND rev_timestamp > NOW() - INTERVAL 1 DAY
+AND rev_timestamp > NOW() - INTERVAL 3 DAY
 AND ll_from IS NULL
 ORDER BY rev_timestamp DESC;
