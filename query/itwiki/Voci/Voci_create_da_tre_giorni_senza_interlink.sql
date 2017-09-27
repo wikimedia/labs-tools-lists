b1efe8b1d2249d4e52dc9239f9c6a93edea5df99
alemela <alessiom92@gmail.com>
2017-09-27 18:03:59 +0200

    fixed interval in query

--- query/itwiki/Voci/Voci_create_da_tre_giorni_senza_interlink.sql
+++ query/itwiki/Voci/Voci_create_da_tre_giorni_senza_interlink.sql
@@ -7,5 +7,6 @@ AND page_namespace = 0
 AND page_id = rev_page
 AND rev_parent_id = 0
 AND rev_timestamp > NOW() - INTERVAL 3 DAY
+AND rev_timestamp < NOW() - INTERVAL 2 DAY
 AND ll_from IS NULL
 ORDER BY rev_timestamp DESC;
