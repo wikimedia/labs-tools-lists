cea79d677ec5376f9301fb11422b9c04fc4943a0
Mess <mesand16@gmail.com>
2019-07-03 22:47:42 +0000

    Complete revision on the queries about orphaned pages (because the previous ones didn't include pages linked only by orphaned redirects)
    Change-Id: I848947d53d9a79eb03e3d3dc92efe40d61b2ee03

--- query/itwiki/Voci/Voci_orfane_senza_interlink.sql
+++ query/itwiki/Voci/Voci_orfane_senza_interlink.sql
@@ -4,28 +4,30 @@ FROM page
 WHERE page_namespace = 0
 AND page_is_redirect = 0
 AND page_title NOT IN (
-  SELECT pl_title
+  SELECT DISTINCT pl_title
   FROM pagelinks
   WHERE pl_from_namespace = 0
   AND pl_namespace = 0
   AND pl_title = page_title
   AND pl_from NOT IN
-  (SELECT page_id
+  (SELECT DISTINCT page_id
    FROM page
-   LEFT JOIN pagelinks
-   ON pl_title = page_title
-   AND pl_namespace = page_namespace
    WHERE page_namespace=0
    AND page_is_redirect=1
-   AND pl_namespace IS NULL)
+   AND page_title NOT IN 
+   (SELECT DISTINCT pl_title
+  FROM pagelinks
+  WHERE pl_from_namespace = 0
+  AND pl_namespace = 0
+  AND pl_title = page_title))
   AND pl_from NOT IN
-  (SELECT cl_from
+  (SELECT DISTINCT cl_from
    FROM categorylinks, pagelinks
    WHERE cl_to = pl_title
    AND pl_namespace = 14
    AND pl_from = 4195411)) /* Wikipedia:ConnectivityProjectInternationalization/ArticlesNotFormingValidLinks */
    AND page_id NOT IN
-   (SELECT cl_from
+   (SELECT DISTINCT cl_from
     FROM categorylinks, pagelinks
     WHERE cl_to = pl_title
     AND pl_namespace = 14
