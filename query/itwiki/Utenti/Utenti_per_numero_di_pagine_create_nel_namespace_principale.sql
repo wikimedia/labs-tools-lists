d0272dae338a9a403e8e244255a7670cd4c0880b
Mess <mesand16@gmail.com>
2020-02-26 01:06:14 +0000

    Code fix of query "itwiki/Utenti/Utenti per numero di pagine create nel namespace principale" after MediaWiki database layout update (version 1.31)
    Change-Id: I7a95146af5ba8d245b611751be5da8bfe5a13ac6

--- query/itwiki/Utenti/Utenti_per_numero_di_pagine_create_nel_namespace_principale.sql
+++ query/itwiki/Utenti/Utenti_per_numero_di_pagine_create_nel_namespace_principale.sql
@@ -1,24 +1,24 @@
 USE itwiki_p;
 SET @counter = 0;
-SELECT @counter := @counter +1, pages_created, rev_user_text
-FROM (SELECT COUNT(*) as pages_created, rev_user_text
-FROM page, revision
+SELECT @counter := @counter +1, pages_created, actor_name
+FROM (SELECT COUNT(*) as pages_created, actor_name
+FROM page, revision, actor
 WHERE page_namespace = 0
 AND page_is_redirect = 0
 AND rev_page = page_id
 AND rev_parent_id = 0
-AND rev_user != 0
-AND rev_user NOT IN (
+AND actor_id = rev_actor
+AND actor_user NOT IN (
 SELECT ug_user
 FROM user_groups 
 WHERE ug_group = 'bot')
-AND rev_user_text NOT IN (
+AND actor_name NOT IN (
 SELECT REPLACE(log_title, '_', ' ')
 FROM logging
 WHERE log_type = 'rights'
 AND log_params LIKE '%bot%'
 AND log_params NOT LIKE '%sysop%'
 GROUP BY log_title)
-GROUP BY rev_user_text) sub
+GROUP BY actor_name) sub
 ORDER BY pages_created DESC
-LIMIT 1000
+LIMIT 1000;
