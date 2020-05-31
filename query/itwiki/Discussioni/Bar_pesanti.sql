2a46cc7c394b75330b7861fbe3067382cb05a474
Diego Monti <diegmonti@gmail.com>
2020-05-31 14:42:32 +0200

    Add Bar pesanti

---
+++ query/itwiki/Discussioni/Bar_pesanti.sql
@@ -0,0 +1,6 @@
+USE itwiki_p;
+SELECT CONCAT("# [[Discussioni_progetto:", page_title, "]] - ", page_len)
+FROM pagelinks
+JOIN page ON page_namespace = pl_namespace AND page_title = pl_title
+WHERE pl_from = 77179 AND pl_namespace = 103 AND page_len >= 200000
+ORDER BY page_len DESC;
