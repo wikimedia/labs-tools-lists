006875fa275382ea2f65afe029cbb1a580abe075
bittachi <mesand16@gmail.com>
2023-04-09 17:31:22 +0200

    Update Voci_senza_elemento_wikidata_per_creatore.sql
    SQL query code fix after revision table updates in MediaWiki 1.35

--- query/itwiki/Voci/Voci_senza_elemento_wikidata_per_creatore.sql
+++ query/itwiki/Voci/Voci_senza_elemento_wikidata_per_creatore.sql
@@ -1,6 +1,6 @@
 USE itwiki_p;
-SELECT CONCAT('# [[Utente:', rev_user_text, ']] (', COUNT(*), ')')
-FROM page, revision
+SELECT CONCAT('# [[Utente:', actor_name, ']] (', COUNT(*), ')')
+FROM page, revision JOIN actor ON actor_id = rev_actor
 WHERE page_id NOT IN
 (SELECT pp_page
 FROM page_props
@@ -9,7 +9,7 @@ AND page_namespace = 0
 AND page_is_redirect = 0
 AND page_id = rev_page
 AND rev_parent_id = 0
-GROUP BY rev_user_text
+GROUP BY actor_name
 HAVING COUNT(*) > 1
 ORDER BY COUNT(*) DESC
 LIMIT 1000;
