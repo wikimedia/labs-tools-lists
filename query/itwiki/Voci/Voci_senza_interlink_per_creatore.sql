09b17808021dac25bd5b86204438ed87f0bcd6f6
Mess <mesand16@gmail.com>
2023-04-09 16:05:38 +0000

    SQL query code fix after "revision" table updates starting from MediaWiki 1.35
    Change-Id: I6a75e160356a13b355997b9478ddd483c58b54c9

--- query/itwiki/Voci/Voci_senza_interlink_per_creatore.sql
+++ query/itwiki/Voci/Voci_senza_interlink_per_creatore.sql
@@ -1,13 +1,15 @@
 USE itwiki_p;
-SELECT CONCAT('# [[Utente:', rev_user_text, ']] (', COUNT(*), ')')
+SELECT CONCAT('# [[Utente:', actor_name, ']] (', COUNT(*), ')')
 FROM page, revision
-LEFT JOIN langlinks ON ll_from = rev_page
+JOIN actor ON actor_id = rev_actor
 WHERE page_is_redirect = 0
 AND page_namespace = 0
 AND page_id = rev_page
 AND rev_parent_id = 0
-AND ll_from IS NULL
-GROUP BY rev_user_text
+AND page_id NOT IN
+(SELECT ll_from
+FROM langlinks)
+GROUP BY actor_name
 HAVING COUNT(*) > 1
 ORDER BY COUNT(*) DESC
 LIMIT 1000;
