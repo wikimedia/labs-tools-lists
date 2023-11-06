7f57954f94b1dea33a3ba367c505971994768c13
Mess <mesand16@gmail.com>
2023-11-06 19:48:59 +0000

    SQL query code fix after "revision" table updates starting from MediaWiki 1.35
    Change-Id: I2fc36a299e7252950578c5c278db09559acd5565

--- query/itwiki/Voci/Stub/Stub_che_potrebbero_non_essere_tali.sql
+++ query/itwiki/Voci/Stub/Stub_che_potrebbero_non_essere_tali.sql
@@ -1,8 +1,19 @@
 SELECT CONCAT('# [[', page_title, ']]')
 FROM page
-JOIN templatelinks ON page_id = tl_from
 WHERE page_namespace = 0
-AND tl_title = 'S'
-AND tl_namespace = 10
+AND page_id IN (SELECT tl_from
+FROM templatelinks
+WHERE tl_target_id IN
+(SELECT lt_id
+ FROM linktarget
+ WHERE lt_namespace = 10
+ AND lt_title IN ('S')))
+AND page_id NOT IN (SELECT tl_from
+FROM templatelinks
+WHERE tl_target_id IN
+(SELECT lt_id
+ FROM linktarget
+ WHERE lt_namespace = 10
+ AND lt_title IN ('S_sezione')))
 AND page_len > 10000
 ORDER BY page_len DESC, page_title;
